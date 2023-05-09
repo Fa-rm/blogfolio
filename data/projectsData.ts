ce83968b7f426c9f082d92331280f3fcf14bd2e1
Fred Matias <fredrmatias@gmail.com>
2023-05-09 23:04:16 +0100

    injection

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -4,22 +4,34 @@ const projectsData = [
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/web-banner.png',
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
+    title: 'InCoSynergy Website',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: '/static/images/web-banner.png',
+    href: '/blog/inco',
   },
   {
     title: 'Adidas Sharepoint',
     description: `Internal sharepoint and intranet updates, and team building motion graphic presentation`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/web-banner.png',
     href: '/blog/adidas',
+  },
+  {
+    title: 'BeInCrypto Academy',
+    description: `Upcoming web 3 E learning platform, with a waitlist mechanism to harvest prospect users while under development`,
+    imgSrc: '/static/images/web-banner.png',
+    href: '/blog/beincryptoacademy',
+  },
+  {
+    title: 'BeInCrypto Jobs',
+    description: `Web3 Job Board platform created by BeInCrypto R&D program`,
+    imgSrc: '/static/images/web-banner.png',
+    href: 'https://beincrypto.com/jobs/',
   },
 ]
 
